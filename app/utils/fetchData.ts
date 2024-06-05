1f8cd927be082a0b8e5e76754cf173a807c521fd
Ryan <ryanbprog@gmail.com>
2024-06-05 21:10:40 +0800

    demo fetch added to function

--- app/utils/fetchData.ts
+++ app/utils/fetchData.ts
@@ -1,5 +1,21 @@
-export const fetchData = async (url: String) => {
-  const response = await fetch(url);
-  const jsonData = await response.json();
-  return jsonData;
+const myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", process.env.FOOTBALL_API_KEY);
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+};
+
+export const fetchData = async () => {
+  try {
+    const response = await fetch(
+      "https://v3.football.api-sports.io/status",
+      requestOptions
+    );
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData;
+  } catch (error) {
+    console.error(error);
+  }
 };

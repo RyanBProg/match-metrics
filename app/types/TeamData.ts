e57ef96088ff3483fcaac68ec4b2de8889d86881
Ryan <ryanbprog@gmail.com>
2024-06-06 13:28:19 +0800

    team data types added

---
+++ app/types/TeamData.ts
@@ -0,0 +1,44 @@
+type Params = {
+  id: string;
+  league: string;
+  season: string;
+};
+
+type Paging = {
+  current: number;
+  total: number;
+};
+
+type Team = {
+  id: number;
+  name: string;
+  code: string;
+  country: string;
+  founded: number;
+  national: boolean;
+  logo: string;
+};
+
+type Venue = {
+  id: number;
+  name: string;
+  address: string;
+  city: string;
+  capacity: number;
+  surface: string;
+  image: string;
+};
+
+type Resp = {
+  team: Team;
+  venue: Venue;
+};
+
+type TeamData = {
+  get: string;
+  parameters: Params;
+  errors: string[];
+  results: number;
+  paging: Paging;
+  response: Resp[];
+};
